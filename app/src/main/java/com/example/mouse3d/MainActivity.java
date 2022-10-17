57458fe267a66bb0263e97e789178b056cf8502f
MForystek <micforystek@student.agh.edu.pl>
2022-10-17 23:18:34 +0200

    Added first activity with two buttons displaying simple text on click

--- app/src/main/java/com/example/mouse3d/MainActivity.java
+++ app/src/main/java/com/example/mouse3d/MainActivity.java
@@ -1,37 +1,34 @@
 package com.example.mouse3d;
 
-import android.os.Bundle;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
-import com.example.mouse3d.databinding.ActivityMainBinding;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        Button leftButton = (Button) findViewById(R.id.leftButton);
+        Button rightButton = (Button) findViewById(R.id.rightButton);
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        if (leftButton != null) {
+            leftButton.setOnClickListener((View.OnClickListener) (view ->
+                    Toast.makeText((Context) MainActivity.this, R.string.leftButton, Toast.LENGTH_LONG).show())
+            );
+        }
+
+        if (rightButton != null) {
+            rightButton.setOnClickListener((View.OnClickListener) (view ->
+                    Toast.makeText((Context) MainActivity.this, R.string.rightButton, Toast.LENGTH_LONG).show())
+            );
+        }
+
     }
-
 }
