b4b897e41dc9885f7540bacaaeeb94acc459bae6
MForystek <micforystek@student.agh.edu.pl>
2022-12-14 01:09:39 +0100

    pom cleaning

--- Mouse3D-desktop/src/main/java/com/mouse3d/bluetoothserver/config/BluetoothServerConfig.java
+++ Mouse3D-desktop/src/main/java/com/mouse3d/bluetoothserver/config/BluetoothServerConfig.java
@@ -1,14 +1,10 @@
 package com.mouse3d.bluetoothserver.config;
 
 import lombok.Data;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 import javax.bluetooth.UUID;
 
 
-@ConfigurationProperties(prefix = "bluetooth-server")
-@Component
 @Data
 public class BluetoothServerConfig {
     private String name;
